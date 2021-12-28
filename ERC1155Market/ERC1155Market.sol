1aedbf08539c05f3d9e3191ad458bc83ee984933
Derek J Leong <derekjleong@gmail.com>
2021-12-27 20:20:45 -0500

    Add files via upload

---
+++ ERC1155Market/ERC1155Market.sol
@@ -0,0 +1,465 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract ERC1155Market is
+    ReentrancyGuard,
+    Initializable,
+    ERC1155Upgradeable,
+    OwnableUpgradeable,
+    ERC1155BurnableUpgradeable,
+    ERC1155SupplyUpgradeable
+{
+    // structs and variables
+    // declare and init counters
+    using Counters for Counters.Counter;
+    Counters.Counter private _collectionIds;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsForSale;
+    Counters.Counter private _itemsSold;
+    // declare contract address, later set in the constructer
+    address payable public contractAddress;
+    // market fee structure
+    uint256 public listingPrice = 0.0025 ether;
+    uint256 public mintingPrice = 0.0001 ether;
+    // ipfs uris for app
+    string public ipfsUri = "https://ipfs.infura.io/ipfs/{id}.json";
+    string public ipfsUriBase = "https://ipfs.infura.io/ipfs/";
+    // struct for items and collections placed on the market
+    struct MarketItem {
+        address payable seller;
+        address payable owner;
+        bool forSale;
+        bool sold;
+        uint256 itemId;
+        uint256 tokenId;
+        uint256 price;
+    }
+    struct Collection {
+        address payable owner;
+        uint256 collectionId;
+        uint256 tokenId;
+        uint256 collectionItemsSold;
+        uint256 itemsInCollection;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {
+        contractAddress = payable(msg.sender);
+    }
+
+    function initialize() public initializer {
+        __ERC1155_init(ipfsUri);
+        __Ownable_init();
+        __ERC1155Burnable_init();
+        __ERC1155Supply_init();
+    }
+
+    // mappings
+    mapping(uint256 => MarketItem) private idToItem;
+    mapping(uint256 => Collection) private idToCollection;
+    mapping(uint256 => string) private _uris;
+    mapping(uint256 => mapping(uint256 => uint256))
+        private IdToCollectionToItems;
+
+    // events
+    event MarketItemCreated(
+        address seller,
+        address owner,
+        bool forSale,
+        bool sold,
+        uint256 indexed itemId,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event CollectionCreated(
+        address owner,
+        uint256 indexed collectionId,
+        uint256 indexed tokenId,
+        uint256 collectionItemsSold,
+        uint256 itemsInCollection
+    );
+
+    // Create market item with minted token
+    function createItem(uint256 tokenId, uint256 price)
+        public
+        payable
+        nonReentrant
+        returns (uint256)
+    {
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+        uint256 itemPrice = 0;
+        if (price > 0) {
+            itemPrice = price;
+        }
+
+        idToItem[itemId] = MarketItem(
+            payable(msg.sender),
+            payable(address(0)),
+            false,
+            false,
+            itemId,
+            tokenId,
+            price
+        );
+
+        safeTransferFrom(msg.sender, address(this), tokenId, 1, "");
+
+        emit MarketItemCreated(
+            msg.sender,
+            address(0),
+            false,
+            false,
+            itemId,
+            tokenId,
+            price
+        );
+        return itemId;
+    }
+
+    // Mints collection token and adds collection to market
+    function createCollection(uint256 tokenId) public returns (uint256) {
+        _collectionIds.increment();
+        uint256 collectionId = _itemIds.current();
+
+        idToCollection[collectionId] = Collection(
+            payable(msg.sender),
+            collectionId,
+            tokenId,
+            0,
+            0
+        );
+
+        safeTransferFrom(msg.sender, address(this), tokenId, 1, "");
+
+        emit CollectionCreated(msg.sender, collectionId, tokenId, 0, 0);
+        return collectionId;
+    }
+
+    // Removes an itemId to collectionsIds associated with a Collection
+    function addItemToCollection(uint256 collectionId, uint256 itemId)
+        public
+        nonReentrant
+    {
+        require(
+            idToCollection[collectionId].owner == msg.sender,
+            "Must be collection owner"
+        );
+
+        IdToCollectionToItems[collectionId][itemId] = itemId;
+        idToCollection[collectionId].itemsInCollection++;
+    }
+
+    // Adds an itemId to collectionsIds associated with a Collection
+    function removeItemFromCollection(uint256 collectionId, uint256 itemId)
+        public
+        nonReentrant
+    {
+        require(
+            IdToCollectionToItems[collectionId][itemId] >= 0, //TODO this may not be good enough
+            "Item doesnt exist in collection"
+        );
+        require(
+            idToCollection[collectionId].owner == msg.sender,
+            "Must be collection owner"
+        );
+
+        delete IdToCollectionToItems[collectionId][itemId];
+        idToCollection[collectionId].itemsInCollection--;
+    }
+
+    // Gets collection items at passed collectionId and itemid
+    function getCollectionItem(uint256 collectionId, uint256 itemId)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            IdToCollectionToItems[collectionId][itemId] >= 0,
+            "Collection item doesnt exist"
+        );
+
+        return IdToCollectionToItems[collectionId][itemId];
+    }
+
+    // Gets collection at passed collectionId
+    function getCollection(uint256 collectionId)
+        public
+        view
+        returns (Collection memory)
+    {
+        require(
+            idToCollection[collectionId].collectionId >= 0,
+            "Collection doesnt exist"
+        );
+
+        return idToCollection[collectionId];
+    }
+
+    // Gets collection at passed collectionId
+    function getAmountOfCollectionItems(uint256 collectionId)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            idToCollection[collectionId].collectionId >= 0,
+            "Collection doesnt exist"
+        );
+
+        return idToCollection[collectionId].itemsInCollection;
+    }
+
+    // Gets item at passed itemId
+    function getItem(uint256 id) public view returns (MarketItem memory) {
+        require(idToItem[id].itemId > 0, "Item doesnt exist");
+
+        return idToItem[id];
+    }
+
+    // Lists Market Item for Sale
+    function listItemForSale(uint256 itemId, uint256 price)
+        public
+        payable
+        nonReentrant
+    {
+        // bool forSaleStatus = idToItem[itemId].forSale;
+        require(msg.sender == idToItem[itemId].seller, "Must be item seller");
+        require(idToItem[itemId].forSale != true, "Item already for sale");
+        require(price > 0, "Price must be at least 1wei");
+        require(msg.value == listingPrice, "Fee must equal listing price");
+
+        idToItem[itemId].forSale = true;
+        idToItem[itemId].price = price;
+        _itemsForSale.increment();
+    }
+
+    // Removes Market Item from Sale Listings
+    function removeItemSaleListing(uint256 itemId) public payable nonReentrant {
+        // bool forSaleStatus = idToItem[itemId].forSale;
+        require(msg.sender == idToItem[itemId].seller, "Must be item seller");
+        require(idToItem[itemId].forSale != false, "Item already NOT for sale");
+        require(msg.value == listingPrice, "Fee must equal listing price");
+
+        idToItem[itemId].forSale = false;
+        _itemsForSale.decrement();
+    }
+
+    // Execute sale of listed market item
+    // transfer token ownership and payment
+    function executeSale(uint256 itemId) public payable nonReentrant {
+        uint256 price = idToItem[itemId].price;
+        uint256 tokenId = idToItem[itemId].tokenId;
+        require(msg.value == price, "Asking price required");
+        // (address(this), msg.sender, tokenId, 1, "");, "Asking price required");
+
+        idToItem[itemId].owner = payable(msg.sender);
+        idToItem[itemId].sold = true;
+        idToItem[itemId].seller.transfer(msg.value);
+        this.safeTransferFrom(address(this), msg.sender, tokenId, 1, "");
+        _itemsSold.increment();
+        payable(contractAddress).transfer(listingPrice);
+    }
+
+    // Fetchs all items on the market
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint256 itemCount = _tokenIds.current();
+        uint256 unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint256 i = 0; i < itemCount; i++) {
+            if (idToItem[i + 1].owner == address(0)) {
+                uint256 currentId = i + 1;
+                MarketItem storage currentItem = idToItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    // Fetchs all collections
+    function fetchMarketCollections()
+        public
+        view
+        returns (Collection[] memory)
+    {
+        uint256 collectionCount = _collectionIds.current();
+        Collection[] memory allCollections = new Collection[](collectionCount);
+        for (uint256 i = 0; i < collectionCount; i++) {
+            uint256 currentId = i + 1;
+            Collection storage currentItem = idToCollection[currentId];
+            allCollections[i] = currentItem;
+        }
+        return allCollections;
+    }
+
+    // Fetchs collections owned by sender
+    function fetchMyCollections() public view returns (Collection[] memory) {
+        uint256 totalItemCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToCollection[i].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        Collection[] memory myCollections = new Collection[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToCollection[i].owner == msg.sender) {
+                Collection storage currentItem = idToCollection[i];
+                myCollections[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return myCollections;
+    }
+
+    // Fetchs market items owned by sender
+    function fetchMyItems() public view returns (MarketItem[] memory) {
+        uint256 totalItemCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (
+                idToItem[i + 1].seller == msg.sender ||
+                idToItem[i + 1].owner == msg.sender
+            ) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory myItems = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (
+                idToItem[i + 1].seller == msg.sender ||
+                idToItem[i + 1].owner == msg.sender
+            ) {
+                uint256 currentId = i + 1;
+                MarketItem storage currentItem = idToItem[currentId];
+                myItems[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return myItems;
+    }
+
+    // Returns uri of token id, for fetching metaData
+    function contractOwner() public view returns (address) {
+        return contractAddress;
+    }
+
+    // Returns uri of token id, for fetching metaData
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return (
+            string(
+                abi.encodePacked(
+                    ipfsUriBase,
+                    Strings.toString(tokenId),
+                    ".json"
+                )
+            )
+        );
+    }
+
+    // Set env ipfsUri varaible and sets uri
+    // should be syncd with ipfsUriBase
+    function setURI(string memory newUri) public {
+        ipfsUri = newUri;
+        _setURI(newUri);
+    }
+
+    // Set env ipfsUriBase varaible and sets uri
+    // should be syncd with ipfsUri
+    function setURIBase(string memory newUriBase) public {
+        ipfsUriBase = newUriBase;
+    }
+
+    // Sets uri associated with a token
+    function setTokenURI(uint256 tokenId, string memory tokenUri) public {
+        require(bytes(_uris[tokenId]).length == 0, "URI can only be set once");
+
+        _uris[tokenId] = tokenUri;
+    }
+
+    // Used to get market listingPrice
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    // Used to get market mintingPrice
+    function getMintingPrice() public view returns (uint256) {
+        return mintingPrice;
+    }
+
+    // Mint new erc1155 token; non-fungible or semi-fungible
+    function mint(
+        uint256 amount,
+        string memory tokenUri,
+        bytes memory data
+    ) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId, amount, data);
+        setTokenURI(newItemId, tokenUri);
+        return newItemId;
+    }
+
+    // Todo
+    // function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    //     public
+    // {
+    //     _tokenIds.increment();
+    //     uint256 newItemId = _tokenIds.current();
+    //     _setTokenURI(newItemId, newuri);
+    //     _mintBatch(to, ids, amounts, data);
+    // }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155Upgradeable, ERC1155SupplyUpgradeable) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
---
+++ ERC1155Market/tests/market.test.js
@@ -0,0 +1,181 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const BLANK_ADDRESS = "0x0000000000000000000000000000000000000000";
+const contractFileName = "HotSwapMarket";
+const NON_FUNGIBLE = 1;
+let MarketContract;
+let hardhatMarket;
+let owner;
+let addr1;
+let addr2;
+let addrs;
+let listingPrice;
+let mintingPrice;
+
+// before() is run once before all the tests in a describe
+// after()   is run once after all the tests in a describe
+// beforeEach() is run before each test in a describe
+// afterEach()   is run after each test in a describe
+
+beforeEach(async () => {
+   // Get the ContractFactory and Signers here.
+   MarketContract = await ethers.getContractFactory(contractFileName);
+   [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+   hardhatMarket = await MarketContract.deploy();
+});
+describe("Market Contract Deployment", () => {
+   it("Should set the correct owner", async () => {
+      const contractOwner = await hardhatMarket.contractOwner();
+      expect(contractOwner).to.equal(owner.address);
+   });
+   // it("Contract Migrates Correctly", async () => {});
+});
+describe("Market Contract Transactions", () => {
+   const tokenUri = "https://www.myimagelocation1.com";
+   const metaDataUri = "https://www.myimagelocation1.com";
+
+   it("Can get fee structure", async () => {
+      listingPrice = await hardhatMarket.getListingPrice();
+      mintingPrice = await hardhatMarket.getMintingPrice();
+      expect(!!listingPrice).to.equal(true);
+      expect(!!mintingPrice).to.equal(true);
+      // listingPrice = ethers.utils.parseUnits(listingPrice, "ether");
+      // mintingPrice = ethers.utils.parseUnits(mintingPrice, "ether");
+      console.log("TESTING", listingPrice, mintingPrice);
+   });
+
+   describe("ERC1155 Token Creation", () => {
+      it("Can mint non-fungible tokens", async () => {
+         const supply = 1;
+         let transaction = await hardhatMarket.mint(supply, metaDataUri, []);
+         transaction = await transaction.wait();
+         expect(transaction.events[0].args.value).to.equal(supply);
+      });
+      it("Can mint semi-fungible tokens", async () => {
+         const supply = 1000;
+         let transaction = await hardhatMarket.mint(supply, metaDataUri, []);
+         transaction = await transaction.wait();
+         expect(transaction.events[0].args.value).to.equal(supply);
+      });
+      it("Can set token URI only once", async () => {
+         const supply = 1;
+         let transaction = await hardhatMarket.mint(supply, metaDataUri, []);
+         transaction = await transaction.wait();
+
+         let success;
+         try {
+            await hardhatMarket.setTokenURI(1, metaDataUri);
+            success = true;
+         } catch {
+            success = false;
+         }
+         expect(success).to.equal(false);
+      });
+   });
+
+   describe("Collections", () => {
+      let transaction;
+      beforeEach(async () => {
+         await hardhatMarket.mint(NON_FUNGIBLE, tokenUri, []);
+         transaction = await hardhatMarket.createCollection(1);
+         transaction = await transaction.wait();
+      });
+      it("Can be created", async () => {
+         expect(transaction.events[1].args.length).to.equal(5);
+      });
+      it("Can be retrieved by id", async () => {
+         const id = 0;
+         const createdCollection = await hardhatMarket.getCollection(id);
+         expect(createdCollection.collectionId).to.equal(id);
+      });
+      it("Can add items to collections", async () => {
+         const id = 0;
+         const itemId = 424242;
+         await hardhatMarket.connect(owner).addItemToCollection(id, itemId);
+         const collectionWithItem = await hardhatMarket.getCollection(id);
+         expect(collectionWithItem.itemsInCollection).to.equal(1);
+      });
+      it("Can retrieve all collections", async () => {
+         const collections = await hardhatMarket.fetchMarketCollections();
+         expect(collections.length).to.equal(1);
+      });
+      it("Can retrieve all collections mapped to sender address", async () => {
+         const collections = await hardhatMarket.fetchMyCollections();
+         expect(collections.length).to.equal(1);
+      });
+   });
+
+   describe("Items", async () => {
+      let transaction;
+      let _itemId;
+      const tokenPrice = 42;
+      beforeEach(async () => {
+         await hardhatMarket.mint(NON_FUNGIBLE, tokenUri, []);
+         transaction = await hardhatMarket.createItem(1, tokenPrice);
+         transaction = await transaction.wait();
+      });
+      it("Can be created", async () => {
+         const [
+            sellerAddress,
+            ownerAddress,
+            forSale,
+            sold,
+            itemId,
+            tokenId,
+            price,
+         ] = transaction.events[1].args;
+
+         expect(sold).to.equal(false);
+         expect(forSale).to.equal(false);
+         expect(sellerAddress).to.equal(owner.address);
+         expect(ownerAddress).to.equal(BLANK_ADDRESS);
+         expect(itemId).to.equal(1);
+         expect(tokenId).to.equal(1);
+         expect(price).to.equal(tokenPrice);
+         _itemId = itemId;
+      });
+      it("Can be retrieved by id", async () => {
+         const fetchedItem = await hardhatMarket.getItem(_itemId);
+         // Expect created collection id to be the first index in our map
+         expect(fetchedItem.itemId).to.equal(_itemId);
+      });
+      it("Can be added or removed from sale", async () => {
+         // List for sale, then fetch the item and check for sale status
+         await hardhatMarket.listItemForSale(_itemId, tokenPrice, {
+            value: listingPrice,
+         });
+         let fetchedItem = await hardhatMarket.getItem(_itemId);
+         expect(fetchedItem.forSale).to.equal(true);
+
+         // remove sale listing, then fetch the item and check for sale status
+         await hardhatMarket.removeItemSaleListing(_itemId, {
+            value: listingPrice,
+         });
+         fetchedItem = await hardhatMarket.getItem(_itemId);
+         expect(fetchedItem.forSale).to.equal(false);
+      });
+      it("Item sales execute correctly", async () => {
+         await hardhatMarket.listItemForSale(_itemId, tokenPrice, {
+            value: listingPrice,
+         });
+         let fetchedItem = await hardhatMarket.getItem(_itemId);
+         expect(fetchedItem.forSale).to.equal(true);
+         expect(fetchedItem.sold).to.equal(false);
+         await hardhatMarket.connect(addr1).executeSale(_itemId, {
+            value: tokenPrice,
+         });
+         fetchedItem = await hardhatMarket.getItem(_itemId);
+         expect(fetchedItem.forSale).to.equal(true);
+         expect(fetchedItem.sold).to.equal(true);
+      });
+      it("Can retrieve all Items on market", async () => {
+         const items = await hardhatMarket.fetchMarketItems();
+         expect(items.length).to.equal(1);
+      });
+      it("Can retrieve all collections mapped to sender address", async () => {
+         const collections = await hardhatMarket.fetchMyItems();
+         expect(collections.length).to.equal(1);
+      });
+   });
+});
